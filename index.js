9d773c35839abc3eb8d83f15f92da389b509574e
Jesse Korff <jessekorff@gmail.com>
2026-05-15 11:22:33 -0400

    add regex metadata extraction and rename route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const uploadRoute = require('./routes/upload');
+const renameRoute = require('./routes/rename');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/upload', uploadRoute);
-
+app.use('/rename', renameRoute);
 // Health check
 app.get('/', (req, res) => {
   res.json({ status: 'Dateline is running' });
