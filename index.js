c72f1811bea90563f9d20260fce3106107f96f28
Jesse Korff <jessekorff@gmail.com>
2026-05-19 13:19:45 -0400

    restrict CORS to Vercel frontend URL

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const downloadAllRoute = require('./routes/downloadAll');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://dateline-addh4wnku-jk-austins-projects.vercel.app'
+}));
 app.use(express.json({ limit: '250mb' }));
 app.use(express.urlencoded({ limit: '250mb', extended: true }));
 
